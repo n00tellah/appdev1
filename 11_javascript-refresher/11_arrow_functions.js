c45fa751c93cced7b71f44fd29ee87c6d5908d99
Your Estela <jalacestelamae@gmail.com>
2025-09-06 13:49:39 +0800

    Add arrow functions activity

---
+++ 11_javascript-refresher/11_arrow_functions.js
@@ -0,0 +1,19 @@
+
+greet = (name) => {
+  return "Hello, " + name;
+}
+
+// Arrow
+square = (n) => {
+  return n * n;
+}
+
+// Arrow with implicit return
+sayHi = () => {
+  console.log("Hi!");
+}
+
+
+console.log(greet("Estela"))
+console.log(square(2))
+sayHi()
