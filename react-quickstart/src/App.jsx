6a36c24355a0925f9937de68047506a6867a3c92
Your Estela <jalacestelamae@gmail.com>
2025-09-17 15:24:23 +0800

    Done writing markup with JSX

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -9,11 +9,21 @@ function MyButton() {
   );
 }
 
+function AboutPage() {
+  return (
+    <>
+      <h1>About</h1>
+      <p>Hello there.<br />How do you do?</p>
+    </>
+  );
+}
+
 export default function App() {
   return (
     <div>
       <h1>Welcome to my app</h1>
       <MyButton />
+      <AboutPage />
     </div>
   );
 }
