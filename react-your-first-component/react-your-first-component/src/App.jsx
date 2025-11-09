54443fd62189aeca3691400708501909710b3729
Your Estela <jalacestelamae@gmail.com>
2025-11-09 12:57:21 +0800

    Done with using a component

--- react-your-first-component/react-your-first-component/src/App.jsx
+++ react-your-first-component/react-your-first-component/src/App.jsx
@@ -4,11 +4,22 @@ import { useState } from 'react'
 //import './App.css'
 
 
-export default function Profile() {
+function Profile() {
   return (
     <img
-      src="https://i.imgur.com/MK3eW3Am.jpg"
+      src="https://i.imgur.com/MK3eW3As.jpg"
       alt="Katherine Johnson"
     />
-  )
+  );
+}
+
+export default function Gallery() {
+  return (
+    <section>
+      <h1>Amazing scientists</h1>
+      <Profile />
+      <Profile />
+      <Profile />
+    </section>
+  );
 }
