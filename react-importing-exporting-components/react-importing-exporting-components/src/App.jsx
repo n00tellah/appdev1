8054d5058da48ec78ec20c57ad121d9b5a981bae
Your Estela <jalacestelamae@gmail.com>
2025-11-09 13:09:45 +0800

    Done with exporting and importing a component

--- react-importing-exporting-components/react-importing-exporting-components/src/App.jsx
+++ react-importing-exporting-components/react-importing-exporting-components/src/App.jsx
@@ -1,24 +1,7 @@
-import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
+import Gallery from './Gallery.jsx';
 
-function Profile() {
+export default function App() {
   return (
-    <img
-      src="https://i.imgur.com/MK3eW3As.jpg"
-      alt="Katherine Johnson"
-    />
-  );
-}
-
-export default function Gallery() {
-  return (
-    <section>
-      <h1>Amazing scientists</h1>
-      <Profile />
-      <Profile />
-      <Profile />
-    </section>
+    <Gallery />
   );
 }
