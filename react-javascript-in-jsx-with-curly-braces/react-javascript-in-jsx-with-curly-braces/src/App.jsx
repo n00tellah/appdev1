43da6bd63c4fb2d3a3c3db3b38e9f81fcbb28554
Your Estela <jalacestelamae@gmail.com>
2025-11-22 10:17:16 +0800

    Using curly braces: A window into the JavaScript world

--- react-javascript-in-jsx-with-curly-braces/react-javascript-in-jsx-with-curly-braces/src/App.jsx
+++ react-javascript-in-jsx-with-curly-braces/react-javascript-in-jsx-with-curly-braces/src/App.jsx
@@ -1,12 +1,9 @@
-export default function Avatar() {
-  const avatar = 'https://i.imgur.com/7vQD0fPs.jpg';
-  const description = 'Gregorio Y. Zara';
+export default function TodoList() {
+  const name = 'Gregorio Y. Zara';
   return (
-    <img
-      className="avatar"
-      src={avatar}
-      alt={description}
-    />
+    <h1>{name}'s To Do List</h1>
   );
 }
+
+
 
