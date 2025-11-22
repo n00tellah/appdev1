b022e46b68bb65cefc8f1ee5fc29ea63cc8b3cca
Your Estela <jalacestelamae@gmail.com>
2025-11-22 10:20:16 +0800

    formatDate

--- react-javascript-in-jsx-with-curly-braces/react-javascript-in-jsx-with-curly-braces/src/App.jsx
+++ react-javascript-in-jsx-with-curly-braces/react-javascript-in-jsx-with-curly-braces/src/App.jsx
@@ -1,7 +1,15 @@
+const today = new Date();
+
+function formatDate(date) {
+  return new Intl.DateTimeFormat(
+    'en-US',
+    { weekday: 'long' }
+  ).format(date);
+}
+
 export default function TodoList() {
-  const name = 'Gregorio Y. Zara';
   return (
-    <h1>{name}'s To Do List</h1>
+    <h1>To Do List for {formatDate(today)}</h1>
   );
 }
 
