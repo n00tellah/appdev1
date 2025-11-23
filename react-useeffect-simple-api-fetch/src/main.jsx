6fff7e805cc24e51fbcad0c3b3fc83eba49b7375
Your Estela <jalacestelamae@gmail.com>
2025-11-23 10:44:11 +0800

    Done with fetching posts data exercise

--- react-useeffect-simple-api-fetch/src/main.jsx
+++ react-useeffect-simple-api-fetch/src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import UsersFetch from './UsersFetch.jsx'
+import PostsFetchAsync from './PostsFetchAsync.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <UsersFetch />
+    <PostsFetchAsync />
   </StrictMode>,
 )
